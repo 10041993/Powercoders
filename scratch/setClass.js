59c1fcf895a3bb1a8a16f2fe513b6245919300e6
Ahmad Al Ahmad <ahmad.alahmad@powercoders.org>
2018-12-14 14:32:28 +0100

    js

---
+++ scratch/setClass.js
@@ -0,0 +1,20 @@
+console.log('My code is running!');
+
+function greet(name) {
+  console.log('Hello, ' + name);
+}
+
+function setClasswarning(event) {
+  let el = document.getElementById('mypara');
+  el.className = 'warning';
+}
+
+function setClasstip(event) {
+  let el = document.getElementById('mypara');
+  el.className = 'tip';
+}
+
+function setClassnormal(event) {
+  let el = document.getElementById('mypara');
+  el.className = 'normal';
+}
