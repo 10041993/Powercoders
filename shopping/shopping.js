0117ae1ed63ae14813b451fadd631f8db42f5da7
Ahmad Al Ahmad <ahmad.alahmad@powercoders.org>
2019-01-10 09:31:30 +0100

    Update shopping

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,7 +1,27 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    document.querySelector('button').addEventListener('click',function (event) {
-        let el = document.querySelector('#item');
-        console.log(el.value);
-    });
+  document.querySelector('button').addEventListener('click', function (event) {
+    let inputBox = document.getElementById('item');
+    let inputValue = inputBox.value;
+    let listitem = createNewListItem(inputValue);
+    let list = document.querySelector('ul');
+    list.appendChild(listitem);
+  });
 });
-
+function createNewListItem(itemText) {
+  console.log(itemText);
+  let li = document.createElement('li');
+  let span = document.createElement('span');
+  let spanText = document.createTextNode(itemText);
+  span.appendChild(spanText);
+  li.appendChild(span);
+  let button = document.createElement('button');
+  let text = document.createTextNode('delete');
+  button.addEventListener('click', function (event) {
+    console.log('item list deleted: ' + itemText);
+    li.remove();
+  });
+  button.appendChild(text);
+  li.appendChild(button);
+  console.log("createElement", li);
+  return li;
+}
