484079e8d0291f701ff12aa4e04dc6615512b5f9
Ahmad Al Ahmad <ahmad.alahmad@powercoders.org>
2019-01-11 16:01:07 +0100

    Update

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,25 +1,31 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    let inputBox = document.getElementById('item');
-    let listItem = document.querySelector('ul');
-
-  document.querySelector('button').addEventListener('click', function (event) {
+  let inputBox = document.getElementById('item');
+  let listItem = document.querySelector('ul');
+  let addItemButton = document.querySelector('button');
+  addItemButton.addEventListener('click', function (event) {
     if (inputBox.value.trim() !== '') {
       listItem.appendChild(createNewListItem(inputBox.value.trim()));
       inputBox.value = '';
+      addItemButton.disabled = true;
     }
     inputBox.focus();
   });
 
-    inputBox.addEventListener('keyup', function (event) {
-      if (inputBox.value.trim() !== ''){
-        if (event.key === 'Enter') {
+  inputBox.addEventListener('keyup', function (event) {
+    if (inputBox.value.trim() !== '') {
+      addItemButton.disabled = false;
+      if (event.key === 'Enter') {
         listItem.appendChild(createNewListItem(inputBox.value.trim()));
-      inputBox.value = '';
+        inputBox.value = '';
       }
+    }
+    if (inputBox.value.trim() === '') {
+      addItemButton.disabled = true;
     }
   });
 
-      inputBox.focus();
+  inputBox.focus();
+  addItemButton.disabled = true;
 });
 
 function createNewListItem(itemText) {
@@ -29,7 +35,7 @@ function createNewListItem(itemText) {
   let spanText = document.createTextNode(itemText);
   span.appendChild(spanText);
   li.appendChild(span);
-  let button = document.createElement('button')
+  let button = document.createElement('button');
   let text = document.createTextNode('delete');
   button.addEventListener('click', function (event) {
     console.log('item list deleted: ' + itemText);
