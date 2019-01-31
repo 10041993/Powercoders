956e51ac845674b9456ef4f5b5c4a3da65b611ad
Ahmad Al Ahmad <ahmad.alahmad@powercoders.org>
2019-01-31 15:04:57 +0100

    Update item

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,9 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
+function domContentLoaded() {
   const inputBox = document.getElementById('item');
   const listItem = document.querySelector('ul');
-  const addItemButton = document.querySelector('#add');
   const clearListButton = document.querySelector('#clear');
+  const addItemButton = document.querySelector('#add');
+  const quaBox = document.getElementById('quantity');
   document.querySelector('#clear').disabled = true;
+
+  inputBox.addEventListener('keyup', onKeyUp);
+  quaBox.addEventListener('keyup', onKeyUp);
 
   clearListButton.addEventListener('click', function () {
     let listItems = document.querySelectorAll('li');
@@ -20,14 +24,19 @@ document.addEventListener('DOMContentLoaded', function () {
       return;
     }
 
-    listItem.appendChild(createNewListItem(trimmedValue));
+    const item = {
+      name: trimmedValue,
+      quantity: quaBox.value
+    };
+
+    listItem.appendChild(createNewListItem(item));
     inputBox.value = '';
     addItemButton.disabled = true;
     clearListButton.disabled = false;
     inputBox.focus();
   });
 
-  inputBox.addEventListener('keyup', function (event) {
+  function onKeyUp() {
     /*Widget that the user types an item in to.*/
     const trimmedValue = inputBox.value.trim();
     addItemButton.disabled = trimmedValue === '';
@@ -38,46 +47,80 @@ document.addEventListener('DOMContentLoaded', function () {
     if (event.key !== 'Enter') {
       return;
     }
+    const item = {
+      name: trimmedValue,
+      quantity: quaBox.value
+    };
 
-    listItem.appendChild(createNewListItem(trimmedValue));
+    listItem.appendChild(createNewListItem(item));
     inputBox.value = '';
     addItemButton.disabled = true;
-    clearListButton.disabled = false;
-  });
-  inputBox.focus();
-  addItemButton.disabled = true;
-
-});
+    clearListButton.disabled = true;
+    inputBox.focus();
+  }
+}
 
 /**
  * Creates and returns an 'li' element for inclusion in the shopping list.
- *
- * @param {string} itemText text of the item to add to the list
+ * @param {{name: string, quantity: string}} item Item to append to the list.
  * @returns {HTMLElement}li element
  */
-function createNewListItem(itemText) {
-  console.log(itemText);
+function createNewListItem(item) {
+  console.log(item.name);
+
   const inputBox = document.getElementById('item');
   const li = document.createElement('li');
-  const span = document.createElement('span');
-  const spanText = document.createTextNode(itemText);
+  const itemSpan = document.createElement('span');
+  const itemSpanText = document.createTextNode(item.name);
 
-  span.appendChild(spanText);
-  li.appendChild(span);
+  itemSpan.appendChild(itemSpanText);
+  li.appendChild(itemSpan);
+
+  if (item.quantity !== '') {
+    li.appendChild(document.createTextNode(' '));
+    const quantitySpan = document.createElement('span');
+    quantitySpan.textContent = `(${item.quantity})`;
+    li.appendChild(quantitySpan);
+  }
 
   const button = document.createElement('button');
   const text = document.createTextNode('delete');
-  const clearListButton = document.querySelector('#clear');
+  button.appendChild(text);
+
   button.addEventListener('click', function () {
-    console.log('item list deleted: ' + itemText);
+    const clearListButton = document.querySelector('#clear');
+    console.log('item list deleted: ' + item.name);
     li.remove();
     inputBox.focus();
     const listItems = document.querySelectorAll('li');
     clearListButton.disabled = listItems.length === 0;
   });
 
-  button.appendChild(text);
   li.appendChild(button);
-  console.log("createElement", li);
   return li;
 }
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', function () {
+    domContentLoaded();
+  });
+} else {
+  domContentLoaded();
+}
+
+const person = {
+  name: {
+    first: 'Ahmad',
+    last: 'Al Ahmad'
+  },
+  interests: ['Teaching', 'Skiing'],
+  greet: function () {
+    console.log(`Hello ${this.name.first}`);
+  }
+};
+
+person.name.first = 'Nicholas';
+person.interests[0] = 'Programming';
+person.interests.push('Teaching');
+person.name = {first: 'Ahmad', last: 'Al Ahmad'};
+person.greet();
